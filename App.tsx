89ca4bb69211bd01d280818659d8dc7d22c389d1
Pastuchov <swjatoslaw.pastuchov@gmx.de>
2021-01-05 18:18:12 +0100

    - buttons with logging

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { LoggerExample } from "./logger/LoggerExample";
+import { loggerFactory } from "./logger/LoggerConfig";
+
+const logUi = loggerFactory.getLogger("ui.App");
 
 export default function App() {
     console.log("-----------------------------------------START");
@@ -10,6 +13,14 @@ export default function App() {
     return (
         <View style={styles.container}>
             <Text>expo playground</Text>
+
+            <TouchableOpacity onPress={() => console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")} style={styles.button}>
+                <Text style={styles.buttonText}>log this</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => logUi.trace("BBBBBBBBBBBBBBBBBBBBBBBBB")} style={styles.button}>
+                <Text style={styles.buttonText}>log this 2</Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -20,5 +31,15 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+    },
+    button: {
+        backgroundColor: "green",
+        padding: 10,
+        borderRadius: 10,
+        margin: 10,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: "#fff",
     },
 });
