e07e228b0ee11ba6ad251911bb153295ad9256c6
Pastuchov <swjatoslaw.pastuchov@gmx.de>
2021-01-05 20:35:34 +0100

    - tested logger typescript-logging

--- App.tsx
+++ App.tsx
@@ -3,23 +3,43 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { LoggerExample } from "./logger/LoggerExample";
 import { loggerFactory } from "./logger/LoggerConfig";
 
-const logUi = loggerFactory.getLogger("ui.App");
+const logger = loggerFactory.getLogger("App");
 
 export default function App() {
-    console.log("-----------------------------------------START");
-    new LoggerExample().doSomeLogs();
-    console.log("-----------------------------------------STOP");
+    // console.log("-----------------------------------------START");
+    // new LoggerExample().doSomeLogs();
+    // console.log("-----------------------------------------STOP");
 
     return (
         <View style={styles.container}>
             <Text>expo playground</Text>
 
-            <TouchableOpacity onPress={() => console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")} style={styles.button}>
-                <Text style={styles.buttonText}>log this</Text>
+            <TouchableOpacity onPress={() => console.log("This is a basic CONSOLE log.")} style={styles.button}>
+                <Text style={styles.buttonText}>log CONSOLE</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => logUi.trace("BBBBBBBBBBBBBBBBBBBBBBBBB")} style={styles.button}>
-                <Text style={styles.buttonText}>log this 2</Text>
+            <TouchableOpacity onPress={() => logger.trace("This is a TRACE log.")} style={styles.button}>
+                <Text style={styles.buttonText}>log TRACE</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => logger.debug("This is a DEBUG log.")} style={styles.button}>
+                <Text style={styles.buttonText}>log DEBUG</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => logger.info("This is an INFO log.")} style={styles.button}>
+                <Text style={styles.buttonText}>log INFO</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => logger.warn("This is a WARN log.")} style={styles.button}>
+                <Text style={styles.buttonText}>log WARN</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => logger.error("This is an ERROR log.")} style={styles.button}>
+                <Text style={styles.buttonText}>log ERROR</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => logger.fatal("This is a FATAL log.")} style={styles.button}>
+                <Text style={styles.buttonText}>log FATAL</Text>
             </TouchableOpacity>
         </View>
     );
@@ -36,7 +56,7 @@ const styles = StyleSheet.create({
         backgroundColor: "green",
         padding: 10,
         borderRadius: 10,
-        margin: 10,
+        margin: 3,
     },
     buttonText: {
         fontSize: 20,
--- logger/LoggerConfig.ts
+++ logger/LoggerConfig.ts
@@ -1,9 +1,12 @@
 import { LoggerFactoryOptions, LogGroupRule, LogLevel, LoggerFactory, LFService } from "typescript-logging";
 
+// NPM: https://www.npmjs.com/package/typescript-logging
+// GitHub: https://github.com/vauxite-org/typescript-logging
 // Doc: https://github.com/vauxite-org/typescript-logging/blob/HEAD/docs/latest_log4j.md
 
 const loggerOptions = new LoggerFactoryOptions();
-loggerOptions.addLogGroupRule(new LogGroupRule(new RegExp("service.+"), LogLevel.Trace));
-loggerOptions.addLogGroupRule(new LogGroupRule(new RegExp("ui.+"), LogLevel.Trace));
+// loggerOptions.addLogGroupRule(new LogGroupRule(new RegExp("service.+"), LogLevel.Info));
+// loggerOptions.addLogGroupRule(new LogGroupRule(new RegExp("ui.+"), LogLevel.Info));
+loggerOptions.addLogGroupRule(new LogGroupRule(new RegExp(".+"), LogLevel.Info));
 
 export const loggerFactory: LoggerFactory = LFService.createNamedLoggerFactory("IT-REX_logger", loggerOptions);
